98369ca4e9917da32686bd802e466373769e86d9
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2022-03-30 02:24:26 +0900

    [wip]ドロワーメニューの作成

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,48 @@
 import styles from './Header.module.scss'
+import Link from 'next/link'
+import {useState} from 'react'
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const clickToggle = () => {
+    setIsOpen(!isOpen);
+  }
   return(
-    <header className="header">
+    <header className={'header'}>
       <div className="container">
         <div className={styles.flex_wrapper}>
-          <h1 className={styles.logo_title}>Furniture Design</h1>
-          <div className={styles.menu_btn}>
+          <Link href="/">
+            <a><h1 className={styles.logo_title}>Furniture Design</h1></a>
+          </Link>
+          <div className={`${styles.menu_btn} ${isOpen ? 'active':''}`} onClick={clickToggle}>
             <span></span>
           </div>
+          <h2 className={`test ${isOpen ? 'active' : ''}`}>aaa</h2>
         </div>
       </div>
+      <ul className={`${styles.header_nav} ${isOpen ? 'active':''}`}>
+        <li>
+          <Link href='/products'>
+            <a>products</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/about'>
+            <a>about</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/company'>
+            <a>company</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/contact'>
+            <a>contact</a>
+          </Link>
+        </li>  
+      </ul>
     </header>
   )
 }
